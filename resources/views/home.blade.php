4ab5b64f86470243bfaf1bf81079b593fd06b570
Resta5 <muhamadrestha5@gmail.com>
2022-03-15 19:13:23 +0700

    y

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -20,20 +20,6 @@
               <p style="text-align: center;">Total Pendaftar</p>
                <h3 style="text-align: center;">{{DB::table('data_pesertas')->count()}}</h3>
 
-                          <div class="col-lg-3 col-6">
-            <!-- small box -->
-            <div class="small-box bg-warning">
-              <div class="inner">
-                <h3>44</h3>
-
-                <p>User Registrations</p>
-              </div>
-              <div class="icon">
-                <i class="ion ion-person-add"></i>
-              </div>
-              <a href="#" class="small-box-footer">More info <i class="fas fa-arrow-circle-right"></i></a>
-            </div>
-          </div>
               </div>
               <div class="icon">
                 <i class="ion ion-bag"></i>
